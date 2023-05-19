b321e9da086481d66a070e688f21c0d4ea9fb0d2
Suemon <133195360+Hiromitsu-Suetake@users.noreply.github.com>
2023-05-19 16:17:16 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,9 @@ const orderController = require("../controllers/AlbumController");
 // Root/Index Routes
 router.get("/", (req, res, next) => {
   res.json({
-    message: "Call the /albums route to retrieve a list of albums",
+    message:  "Call the /albums route to retrieve a list of albums",
+    message:  "ワークフローの自動実行を確認します"  
+  
   });
 });
 
